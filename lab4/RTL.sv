72fd89c9924d29ee9afce267651ebc7ff0f85656
Ayesha-Qadir-code <ayeshaqadir2252005@gmail.com>
2025-02-13 10:56:57 +0500

    code corrected

--- lab4/RTL.sv
+++ lab4/RTL.sv
@@ -2,8 +2,8 @@
 `timescale 1ns/10ps
 
 module lab04(output x,y,
-            input a,b,c);
-           wire not_out, or1_out, nand_out, or2_out, or3_out;
+           input a,b,c);
+           logic not_out, or1_out, nand_out, or2_out, or3_out;
            assign not_out= ~c;
            assign or1_out= a|b;
            assign or2_out= a|b;
